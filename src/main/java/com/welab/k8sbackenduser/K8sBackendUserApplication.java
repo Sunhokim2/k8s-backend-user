f9d032901f0e09a301b0bf56bef4dcb4ed3333bf
Sunhokim2 <mcwright@naver.com>
2025-06-09 15:23:05 +0900

    Initial commit

---
+++ src/main/java/com/welab/k8sbackenduser/K8sBackendUserApplication.java
@@ -0,0 +1,13 @@
+package com.welab.k8sbackenduser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class K8sBackendUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(K8sBackendUserApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/welab/k8sbackenduser/K8sBackendUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.welab.k8sbackenduser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class K8sBackendUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
